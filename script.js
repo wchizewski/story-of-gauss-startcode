933650bc80fb22ec718f2711d364e45770c2b645
wchizewski <w.chizewski@share.epsb.ca>
2021-10-14 14:10:22 -0600

    fnished

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
 let calcBtn = document.getElementById("calc-btn");
 let sumOut = document.getElementById("sum-out");
 let nIn = document.getElementById("n-in");
+nOut = document.getElementById("n-out");
 
 // add event listener
 calcBtn.addEventListener("click", calcSum);
@@ -15,23 +16,30 @@ function input(){
     // get n value
     let nValue = +nIn.value;
     // check n value
-    if (nValue < 1){
+    // min
+    if (nValue < 0){
         nValue = 0
-        nIn = 0
-    }else if (nValue > 10000){
+        nIn.value = 1
+    // max
+    } else if (nValue > 10000){
         nValue = 10000;
         nIn.value = 10000;
     }
 }
-
-function calcSum() {
     
+function calcSum() {
+    // get n value
+    let nValue = +nIn.value;
+    // change ? to n
+    nOut.innerHTML = nIn.value
 
-    // Sum integers 1 to 100
-    let total = 0;
-    for (let n = 1; n <= 100; n++) {
-        total = total + n;
-    }
+    // // Sum integers 1 to 100
+    // let total = 0;
+    // for (let n = 1; n <= nIn.value; n++) {
+    //     total = total + n;
+    // }
+
+    let total = nValue * ( nValue + 1 ) / 2
 
     // Output the Total
     sumOut.innerHTML = total
